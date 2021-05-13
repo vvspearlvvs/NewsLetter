171b178e698b43ad36c544238b8e2eaf0c1ff3b5
jinju <gg66477@gmail.com>
2021-05-14 02:13:23 +0900

    update:sqlite->mysql

--- Front/MysqlDB.py
+++ Front/MysqlDB.py
@@ -1,4 +1,4 @@
-import pymsql
+import pymysql
 
 #mongodb
 mysql_host = 'localhost'
@@ -6,17 +6,17 @@ mysql_user = 'root'
 mysql_pwd = ''
 mysql_db = 'mydb'
 
-mysql_client = pymsql.connect(user=mysql_user,passwd=mysql_pwd,host=mysql_host,db=mysql_db)
+mysql_client = pymysql.connect(user=mysql_user,passwd=mysql_pwd,host=mysql_host,db=mysql_db)
 
 #input email data
 def insert_email(value1,value2):
-    cursor = mysql_client.cursor(pymsql.cursors.Cursor)
+    cursor = mysql_client.cursor(pymysql.cursors.Cursor)
     input_sql="INSERT INTO emails values(?,?);"
     cursor.execute(input_sql,(value1,value2))
 
 def find_email():
     email_list = []
-    cursor = mysql_client.cursor(pymsql.cursors.Cursor)
+    cursor = mysql_client.cursor(pymysql.cursors.Cursor)
     cursor.execute("Select email FROM emails")
     receivers = cursor.fetchall()
     for receiver in receivers:
