54f0b0335a55c80e70a0a7d46f702e2e45ab788b
jinju <gg66477@gmail.com>
2021-05-14 02:11:06 +0900

    update:sqlite->mysql

--- Front/app.py
+++ Front/app.py
@@ -1,26 +1,11 @@
 import datetime
 import sqlite3
+import MysqlDB
 from flask import Flask,render_template,request
 app = Flask(__name__)
 
 now=datetime.datetime.now()
 nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
-
-def database(value1,value2):
-    email_list=[]
-    conn = sqlite3.connect("emaildb.db", isolation_level=None) #DB생성
-    c=conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS emails(email varchar(256) PRIMARY KEY, sub_date datetime)")
-    input_sql="INSERT INTO emails values(?,?);"
-    c.execute(input_sql,(value1,value2))
-
-    c.execute("Select email FROM emails")
-    receivers = c.fetchall()
-    for receiver in receivers:
-        email_list.append(receiver[0])
-    print(email_list)
-    conn.close
-    #print(c.fetchall())
 
 @app.route('/')
 def index():
@@ -29,12 +14,11 @@ def index():
 @app.route('/post',methods=['POST'])
 def post():
     value=request.form['email']
-    database(value,nowDatetime)
-    #Database.insert_email(value)
+    #MongoDB.insert_email(value)
+    MysqlDB.insert_email(value,nowDatetime)
     return render_template("index.html")
 
 #추가로, 올바른 이메일인지 확인하는 검증하는 시스템까지 넣기
-
 #특정시간이 되면 new.py 수행
 
 if __name__ == '__main__':
--- Front/new.py
+++ Front/new.py
@@ -1,36 +1,21 @@
-#import Database
+#import MongoDB
+import MysqlDB
 import sqlite3
 import Emailing
 import Crawling
 
-def Database():
-    email_list=[]
-    conn = sqlite3.connect("emaildb.db", check_same_thread = False, isolation_level=None) #DB생성
-    c=conn.cursor()
-
-    c.execute("Select * FROM emails")
-    receivers = c.fetchall() #list
-    for receiver in receivers:
-        email_list.append(receiver[0])
-    return email_list
-
 def main():
-    #document_list = Crawling.get_crawling_aws() #aws데이터수집
-    #Database.insert_data(document_list) #디비저장
+    document_list = Crawling.get_crawling_aws() #aws데이터수집
+    #MongoDB.insert_data(document_list) #디비저장
 
-    #html = Emailing.create_html(document_list) #메일보낼준비-메일내용생성
+    html = Emailing.create_html(document_list) #메일보낼준비-메일내용생성
 
-    '''
-    email_list = Database.find_email('email') #메일보낼준비-수신자확인
-    for receiver in email_list:
-        email=receiver['email']
-        Emailing.send_email(html,email) #메일전송-
-     '''
-    email_list = Database()
+    #email_list = MongoDB.find_email('email') #메일보낼준비-수신자확인
+    email_list = MysqlDB.find_email()
     for email in email_list:
-        print("구독한 이메일들")
-        print(email)
-        #Emailing.send_email(html,email) #메일전송-
+        #email=receiver['email']
+        Emailing.send_email(html,email) #메일전송-
+
 
 if __name__ == '__main__':
     main()
